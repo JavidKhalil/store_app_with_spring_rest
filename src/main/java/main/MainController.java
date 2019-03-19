6ec0ccb3030fae79a8873630b529cb93055d9933
javid <cavidxalilov@gmail.com>
2019-03-20 02:00:07 +0400

    new rest features

--- src/main/java/main/MainController.java
+++ src/main/java/main/MainController.java
@@ -3,10 +3,13 @@ package main;
 import main.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/demo")
@@ -16,7 +19,7 @@ public class MainController {
     private ProductRepository productRepository;
 
     @GetMapping("/add")
-    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String price){
+    public @ResponseBody String addNewProduct(@RequestParam String name, @RequestParam String price){
         Product product = new Product();
         product.setName(name);
         product.setPrice(price);
@@ -24,8 +27,34 @@ public class MainController {
         return "saved";
     }
 
+    @GetMapping("/delete")
+    public @ResponseBody String deleteProduct(@RequestParam String name){
+        Product product = new Product();
+        product.setName(name);
+        productRepository.delete(product);
+        return "deleted";
+    }
+
+    @GetMapping("/update")
+    public @ResponseBody String updateProduct(@RequestParam String idi, @RequestParam String name, @RequestParam String price){
+        Product product = productRepository.findById(Integer.valueOf(idi)).get();
+        product.setName(name);
+        product.setPrice(price);
+        productRepository.save(product);
+        return "updated";
+    }
+
+    @GetMapping("/select")
+    ///^[0-9]*$
+    public @ResponseBody String selectProduct(@RequestParam String id) {
+        Product product = productRepository.findById(Integer.valueOf(id)).get();
+        return "selected";
+    }
+
     @GetMapping("/all")
-    public @ResponseBody Iterable<Product> getAllUsers(){
-        return productRepository.findAll();
+    public @ResponseBody String getAllUsers(Model model){
+        List<Product> productList = productRepository.findAll();
+        model.addAttribute("products", productList);
+        return "all products selected";
     }
 }
--- src/main/java/main/ProductRepository.java
+++ src/main/java/main/ProductRepository.java
@@ -1,8 +1,8 @@
 package main;
 
 import main.entity.Product;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 }
